38e8b7885c003e26a1fc838c6ff609c5ec0983a5
Angel Almanza <almanza.angel245@gmail.com>
2023-11-06 20:37:04 -0700

    Añadidos los archivos de configuracion para el entorno de desarrollo creados y configurados

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'react/no-unstable-nested-components': 'off',
+    'react-hooks/exhaustive-deps': 0,
+    'react-native/no-inline-styles': 0,
+  },
+};
